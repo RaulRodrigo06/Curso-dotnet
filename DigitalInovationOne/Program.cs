01ccb95a8a19d5829b1077377232c119e4c7a833
RaulRodrigo06 <raul_rodrigo6@live.com>
2021-02-14 13:13:37 -0300

    Criação Rep
    New Rep

---
+++ DigitalInovationOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DigitalInovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodeVezes = 5;
+
+            for (int i = 0; i< numerodeVezes; i++)
+            {
+                Console.WriteLine($"Bem Vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
