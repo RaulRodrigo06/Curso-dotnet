2699fabe956719fbc511c2121be405374facadf7
unknown <raul_rodrigo6@live.com>
2021-02-14 13:01:02 -0300

    new file:   DIO.Bank/Classes/Conta.cs 	new file:   DIO.Bank/DIO.Bank.csproj 	new file:   DIO.Bank/Enum/TipoConta.cs 	new file:   DIO.Bank/Program.cs 	new file:   DIO.Bank/bin/Debug/net5.0/DIO.Bank.deps.json 	new file:   DIO.Bank/bin/Debug/net5.0/DIO.Bank.dll 	new file:   DIO.Bank/bin/Debug/net5.0/DIO.Bank.exe 	new file:   DIO.Bank/bin/Debug/net5.0/DIO.Bank.pdb 	new file:   DIO.Bank/bin/Debug/net5.0/DIO.Bank.runtimeconfig.dev.json 	new file:   DIO.Bank/bin/Debug/net5.0/DIO.Bank.runtimeconfig.json 	new file:   DIO.Bank/bin/Debug/net5.0/ref/DIO.Bank.dll 	new file:   DIO.Bank/obj/DIO.Bank.csproj.nuget.dgspec.json 	new file:   DIO.Bank/obj/DIO.Bank.csproj.nuget.g.props 	new file:   DIO.Bank/obj/DIO.Bank.csproj.nuget.g.targets 	new file:   DIO.Bank/obj/Debug/net5.0/.NETCoreApp,Version=v5.0.AssemblyAttributes.cs 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.AssemblyInfo.cs 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.AssemblyInfoInputs.cache 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.GeneratedMSBuildEditorConfig.editorconfig 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.assets.cache 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.csproj.CoreCompileInputs.cache 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.csproj.FileListAbsolute.txt 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.csprojAssemblyReference.cache 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.dll 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.genruntimeconfig.cache 	new file:   DIO.Bank/obj/Debug/net5.0/DIO.Bank.pdb 	new file:   DIO.Bank/obj/Debug/net5.0/apphost.exe 	new file:   DIO.Bank/obj/Debug/net5.0/ref/DIO.Bank.dll 	new file:   DIO.Bank/obj/project.assets.json 	new file:   DIO.Bank/obj/project.nuget.cache

---
+++ ProjetoDIO/DIO.Bank/Enum/TipoConta.cs
@@ -0,0 +1,9 @@
+namespace DIO.Bank
+{
+    public enum TipoConta
+    {
+        PessoaFísica = 1,
+        PessoaJurídica = 2
+    }
+
+}
---
+++ ProjetoDIO/DIO.Bank/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DIO.Bank
+{
+    class Program
+    {
+        static List<Conta> listContas = new List<Conta>();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while(opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserieContas();
+                        break;
+                    case "3":
+                        Transferir();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                        Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+            Console.WriteLine("Obrigado Por Utilizar nossos serviços!");
+            Console.ReadLine();
+        }
+
+        private static void Transferir()
+        {
+            Console.Write("Digite o número da conta de Origem: ");
+            int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número da conta de Destino: ");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser Transferido: ");
+            double valorTransferência = double.Parse(Console.ReadLine());
+
+            listContas[indiceContaOrigem].Transferir(valorTransferência,listContas[indiceContaDestino]);
+        }
+
+        private static void Depositar()
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser Depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Depositar(valorDeposito);
+        }
+
+        private static void Sacar()
+        {
+            Console.Write("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double ValorSaque = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(ValorSaque);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar Contas");
+            if(listContas.Count==0)
+            {
+                Console.WriteLine("Nenhuma Conta Cadastrada.");
+            }
+
+            for(int i = 0; i<listContas.Count; i++)
+            {
+                Conta conta = listContas[i];
+                Console.Write("#{0} - ",i);
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static void InserieContas()
+        {
+            Console.WriteLine("Inserir uma nova conta:");
+
+            Console.Write("Digite 1 para Conta Física ou 2 para Jurídica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Nome do Cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o Saldo Inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                                    saldo: entradaSaldo,
+                                                    credito: entradaCredito,
+                                                    nome: entradaNome);
+            listContas.Add(novaConta);
+
+        }
+
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO BANK a seu dispor!!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar Contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair"); 
+        
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+}
